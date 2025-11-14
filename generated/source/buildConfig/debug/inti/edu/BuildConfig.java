7d07f833a40ec969683cf12b873d0270e6a13312
Dylan <dngweijien@gmail.com>
2025-11-14 15:13:37 +0800

    Add files via upload

---
+++ generated/source/buildConfig/debug/inti/edu/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package inti.edu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "inti.edu";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GMAIL_APP_PASSWORD = "joperthoazksikhm";
+  // Field from default config.
+  public static final String GMAIL_USER = "codetesters01@gmail.com";
+  // Field from default config.
+  public static final String SENDGRID_API_KEY = "";
+}
